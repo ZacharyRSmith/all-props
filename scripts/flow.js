c17fb831ec7cd2b6a031276153b1a4532977636a
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-02-05 21:09:33 -0500

    feature: transformLeaves()

---
+++ scripts/flow.js
@@ -0,0 +1,6 @@
+// file: scripts/flow.js
+/* eslint-disable camelcase */
+const child_process = require('child_process');
+const dir = process.cwd();
+const client_root = dir.charAt(0).toLowerCase() + dir.slice(1);
+child_process.execSync(`flow status ${client_root}`, {stdio: [0, 1, 2]});
